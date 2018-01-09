ee6be48d0ce2e4cd100853ed7d7d63cf721605c7
Tun Naung Soe <Tun@QPMNET.local>
2018-01-09 16:03:52 +0800

    home page added tun word

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,6 +6,8 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
+<h3>I am TNS</h3>
+
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
